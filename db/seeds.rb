f7013aa5eb26fdc8f8fa897799759437d73c77c1
Marat Oshmiansky <maratical@gmail.com>
2021-06-23 11:55:47 -0700

    add validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.create(first_name: "Jack", last_name: "Sprat", email: "jacksprat@gmail.com")
+user2 = User.create(first_name: "Helga", last_name: "Swedenborg", email: "helgaswedenborg@gmail.com")
+user3 = User.create(first_name: "Dirk", last_name: "Diggler", email: "dirkdiggler@gmail.com")
